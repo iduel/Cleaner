5aebc17f4fc93088b87a64a2e9a302dbd83b36e0
iduel <irixt123@outlook.com>
2022-03-20 17:18:59 +0800

    Added remove all log files in directory C:/

--- main.py
+++ main.py
@@ -14,6 +14,29 @@ class Cleaner(Thread):
         with open("path.json") as r:
             self.paths = json.load(r)[-1]
             
+            
+    def delete_file(self, folder):
+        if not os.path.isdir(folder):
+            return
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception:
+                print("Failed to delete " + file_path)
+            else:
+                print("Successfully deleted " + file_path)
+                
+                
+    def delete_logs(self):
+        print("Cleaning log files")
+        os.chdir("c:/")
+        os.system("del *.log /a /s /q /f")
+                
+            
     def run(self):
         for path in self.paths:
             for name, cache_paths in self.paths[path].items():
@@ -24,17 +47,11 @@ class Cleaner(Thread):
                             folder = self.local + cache_path
                         elif path == "roaming":
                             folder = self.roaming + cache_path
-                        for filename in os.listdir(folder):
-                            file_path = os.path.join(folder, filename)
-                            try:
-                                if os.path.isfile(file_path) or os.path.islink(file_path):
-                                    os.unlink(file_path)
-                                elif os.path.isdir(file_path):
-                                    shutil.rmtree(file_path)
-                            except Exception:
-                                print("Failed to delete " + file_path)
-                            else:
-                                print("Successfully deleted " + file_path)
+                        else:
+                            folder = cache_path
+                        self.delete_file(folder)
+        self.delete_logs()
+        
         
         
 if __name__ == "__main__":
